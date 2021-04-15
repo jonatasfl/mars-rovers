c1dde3890c275b6eb2d400512306682046237682
Jonatas Lizandro <jonatas.exe@gmail.com>
2021-04-15 17:33:08 -0300

    finish plateau model tests

--- src/__tests__/plateau.test.ts
+++ src/__tests__/plateau.test.ts
@@ -1,5 +1,16 @@
+import Plateau from '../models/Plateau';
+
 describe('Test Plateau model', () => {
-  it('should be sum 1 + 1', () => {
-    expect(1 + 1).toBe(2);
+  it('should be defined with valid parameters', () => {
+    expect(new Plateau(5, 5)).toBeDefined();
+  });
+
+  it('should throw error with invalid parameters', () => {
+    expect(() => new Plateau(0, 0)).toThrow(Error);
+  });
+
+  it('should get correct max bounds', () => {
+    const plateau = new Plateau(5, 5);
+    expect(plateau.getMaxBounds()).toEqual({ topRightX: 5, topRightY: 5 });
   });
 });
