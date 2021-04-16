105e951484d4d22578c2d2abf136b8488c06cfbb
Jonatas Lizandro <jonatas.exe@gmail.com>
2021-04-16 01:15:19 -0300

    add textfile model with tests

---
+++ src/__tests__/text-file.test.ts
@@ -0,0 +1,21 @@
+import { resolve } from 'path'
+
+import TextFile from "models/TextFile"
+
+describe('Tests for TextFile model', () => {
+  it('should be defined with correct file path', () => {
+    const path = resolve(__dirname, '..', '..', 'input.txt');
+    expect(new TextFile(path)).toBeDefined();
+  });
+
+  it('should throw error if invalid file pathis provided', () => {
+    const path = resolve(__dirname, '..', '..', 'inputttttt.txt');
+    expect(() => new TextFile(path)).toThrow();
+  });
+
+  it('should be able to get the file contents as array', () => {
+    const path = resolve(__dirname, '..', '..', 'input.txt');
+    const file = new TextFile(path);
+    expect(file.getData()).toEqual(['5 5', 'X X']);
+  });
+})
---
+++ src/models/TextFile.ts
@@ -0,0 +1,18 @@
+import { readFileSync } from 'fs';
+
+export default class TextFile {
+  private data: Array<string> = [];
+
+  constructor(path: string) {
+    try {
+      const data = readFileSync(path);
+      this.data = data.toString().split(/\r?\n/);
+    } catch (e) {
+      throw new e;
+    }
+  }
+
+  public getData(): Array<string> {
+    return this.data;
+  }
+}
